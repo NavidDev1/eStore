1dcbc077d8668a7a7ad2724bade4f4b96b875f8d
Naviddev1 <Navid86@hotmail.se>
2024-01-12 16:10:18 +0100

    installed bcrypt and hashed the password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
+import bcrypt from 'bcryptjs';
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -11,7 +12,11 @@ const createUser = asyncHandler(async (req, res) => {
   const userExist = await User.findOne({ email });
   if (userExist) res.status(400).send('User already exists');
 
-  const newUser = new User({ username, email, password });
+  //creating the salt and hashing the password when creating the user
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
