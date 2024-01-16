f6a48f4280ea4dc7b5966d840c4beb46b85e278c
Naviddev1 <Navid86@hotmail.se>
2024-01-16 11:33:18 +0100

    created Constsants and redux toolkit with apiSlice

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -4,6 +4,7 @@ import {
   AiOutlineShopping,
   AiOutlineUserAdd,
   AiOutlineShoppingCart,
+  AiOutlineLogin,
 } from 'react-icons/ai';
 
 import { FaHeart } from 'react-icons/fa';
@@ -31,7 +32,7 @@ export default function Navigation() {
     <div
       style={{ zIndex: 999 }}
       className={`${showSidebar ? 'hidden' : 'flex'} 
-      xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w.[4%] hover:w-[15%] h-[100vh] fixed`}
+      xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-gradient-to-r from-green-400 to-green-600 w-[4%] hover:w-[15%] h-[100vh] fixed`}
       id='navigation-container'
     >
       <div className='flex flex-col justify-center space-y-4'>
@@ -42,7 +43,55 @@ export default function Navigation() {
           <AiOutlineHome className='mr-2 mt-[3rem]' size={26} />
           <span className='hidden nav-item-name mt-[3rem]'>HOME</span>{' '}
         </Link>
+
+        <Link
+          to='/shop'
+          className='flex items-center transition-transform transform hover:translate-x-2'
+        >
+          <AiOutlineShopping className='mr-2 mt-[3rem]' size={26} />
+          <span className='hidden nav-item-name mt-[3rem]'>SHOP</span>{' '}
+        </Link>
+
+        <Link
+          to='/cart'
+          className='flex items-center transition-transform transform hover:translate-x-2'
+        >
+          <AiOutlineShoppingCart className='mr-2 mt-[3rem]' size={26} />
+          <span className='hidden nav-item-name mt-[3rem]'>CART</span>{' '}
+        </Link>
+
+        <Link
+          to='/favorite'
+          className='flex items-center transition-transform transform hover:translate-x-2'
+        >
+          <FaHeart className='mr-2 mt-[3rem]' size={26} />
+          <span className='hidden nav-item-name mt-[3rem]'>Favorite</span>{' '}
+        </Link>
       </div>
+
+      <ul>
+        <li>
+          <Link
+            to='/login'
+            className='flex items-center transition-transform transform hover:translate-x-2'
+          >
+            <AiOutlineLogin className='mr-2 mt-[3rem]' size={26} />
+            <span className='hidden nav-item-name mt-[3rem]'>Login</span>{' '}
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to='/register'
+            className='flex items-center transition-transform transform hover:translate-x-2'
+          >
+            <AiOutlineUserAdd className='mr-2 mt-[3rem]' size={26} />
+            <span className='hidden nav-item-name mt-[3rem]'>
+              Register
+            </span>{' '}
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ frontend/src/redux/api/usersApiSlice.js
@@ -0,0 +1,16 @@
+import { apiSlice } from './apiSlice';
+import { USERS_URL } from '../features/constants';
+
+// Create a new API slice specific to the 'users' domain by injecting endpoints
+// Define the endpoints for the 'users' domain using the injectEndpoints method
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
