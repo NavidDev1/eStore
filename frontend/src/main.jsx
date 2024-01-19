6d7803c9110ae00e16c1b090546df14b380b6ff0
Naviddev1 <Navid86@hotmail.se>
2024-01-19 16:15:32 +0100

    some fixes to the main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,15 +10,19 @@ import store from './redux/features/store.js';
 
 //Auth
 import Login from './pages/Auth/Login.jsx';
+import Register from './pages/Auth/Register.jsx';
 
+// Setting up React Router and rendering the application
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Route>
   )
 );
 
+// Rendering the React application with Redux store and React Router integration
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <RouterProvider router={router} />
