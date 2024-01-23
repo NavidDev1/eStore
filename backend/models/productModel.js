681cbd2a2eb514a5f1c86ec4a209dcbdb681367f
Naviddev1 <Navid86@hotmail.se>
2024-01-23 13:26:24 +0100

    created productModel, with review schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -19,4 +19,21 @@ const reviewSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-export default reviewSchema;
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: ObjectId, ref: 'Category', required: true },
+    description: { type: String, required: true },
+    reviews: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
