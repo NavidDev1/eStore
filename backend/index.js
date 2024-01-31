a1d9192893dd713637ce97757bf1667ed2ffd2a9
Naviddev1 <Navid86@hotmail.se>
2024-01-31 21:45:28 +0100

    paypal and order components done som fixes still needed

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/upload', uploadRoutes);
-app.use('api/orders', orderRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/api/config/paypal', (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
